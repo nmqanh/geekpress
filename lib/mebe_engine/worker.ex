e606ac0d334f4dbfa72f89a6d4ae0ac863666c55
Anh Nguyen <nmqanh@gmail.com>
2016-06-19 11:53:35 +1000

    rename link config to link blog

--- lib/mebe_engine/worker.ex
+++ lib/mebe_engine/worker.ex
@@ -16,7 +16,6 @@ defmodule MebeEngine.Worker do
 
   def init(:ok) do
     load_db
-    System.cmd "ln", ["-fs", @data_path <> "/images", "./priv/static/"]
     if Mix.env == :dev do
       :fs.start_link(:fs_watcher, @data_path)
       :fs.subscribe(:fs_watcher)
